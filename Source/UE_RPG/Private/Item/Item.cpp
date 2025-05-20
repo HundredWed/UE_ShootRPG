ed902527809c4f0e98b73508fa19d0b553348782
kjy <kjy072512@gmail.com>
2025-05-20 22:38:21 +0900

    Item 정보 관리 방법 변경.
    복잡하고 번거러웠던 관리 방식 변경.
    
    그로인한 쓸때없는 변수 삭제

--- Source/UE_RPG/Private/Item/Item.cpp
+++ Source/UE_RPG/Private/Item/Item.cpp
@@ -15,35 +15,7 @@ UItem* UItem::CreateItemCopy()
 {
 	/**the case other get other inventory. not PickUp */
 	UItem* copy = NewObject<UItem>(StaticClass());
-
-	/**item data*/
-	copy->ItemInfoID = this->ItemInfoID;
-	copy->Name = this->Name;
-	copy->Description = this->Description;
-	copy->bCanBeUsed = this->bCanBeUsed;
-	copy->bCanStacked = this->bCanStacked;
-	copy->UseText = this->UseText;
-	copy->Interaction = this->Interaction;
-	copy->ItemPrice = this->ItemPrice;
-	copy->Weight = this->Weight;
-	copy->ATK = this->ATK;
-
-	/**itemtype data*/
-	copy->ItemType = this->ItemType;
-	copy->ConsumeValue = this->ConsumeValue;
-	copy->CombinResultID = this->CombinResultID;
-	if (this->ItemClass)
-	{
-		copy->ItemClass = this->ItemClass;
-	}
-	
-	/**asset data*/
-	copy->ItemMesh = this->ItemMesh;
-	copy->IconTexture = this->IconTexture;
-	copy->ItemSkeletalMesh = this->ItemSkeletalMesh;
-	copy->FireParticle = this->FireParticle;
-
-	copy->bCopyItem = true;
+	copy->ItemInfoTable = this->ItemInfoTable;
 
 	return copy;
 }
--- Source/UE_RPG/Public/Item/Item.h
+++ Source/UE_RPG/Public/Item/Item.h
@@ -16,65 +16,8 @@ public:
 	
 	UItem();
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Data")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
 		FItemInfoTable ItemInfoTable;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Data")
-		FName ItemInfoID;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		FText Name;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		FText Description;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		bool bCanBeUsed;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		bool bCanStacked;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		FText UseText;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		FText Interaction;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		int32 ItemPrice;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		float Weight;
-
-	UPROPERTY(VisibleAnywhere, Category = "ItemType Data")
-		TSubclassOf<AActor> ItemClass;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ItemType Data")
-		EItemCategory ItemType;
-
-	UPROPERTY(EditAnywhere, Category = "ItemType Data")
-		FName CombinResultID;
-
-	UPROPERTY(VisibleAnywhere, Category = "ItemType Data")
-		int32 ConsumeValue;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		int32 ATK;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Data")
-		TSubclassOf<ACPP_WeaponBase> WeaponActor;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Asset Data")
-		UStaticMesh* ItemMesh;
-
-	UPROPERTY(EditAnywhere, Category = "Asset Data")
-		USkeletalMesh* ItemSkeletalMesh;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Asset Data")
-		UTexture2D* IconTexture;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Asset Data")
-		class UParticleSystem* FireParticle;
 
 	bool bCopyItem;
 
